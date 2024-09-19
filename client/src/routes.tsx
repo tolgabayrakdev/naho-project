7aafba39ed5053b088b2c150683d029fa52f8993
tolgabayrakdev <bayraktolga28@gmail.com>
2024-09-19 11:08:16 +0300

    not found page

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -4,6 +4,8 @@ import { lazy } from "react";
 
 const HomePage = lazy(() => import('./pages/home'))
 
+const NotFoundPage = lazy(() => import('./pages/not-found'))
+
 const SignInPage = lazy(() => import('./pages/auth/sign-in'))
 const SignUpPage = lazy(() => import('./pages/auth/sign-up'))
 
@@ -11,6 +13,10 @@ const routes = createBrowserRouter([
     {
         path: "/",
         element: <HomePage />
+    },
+    {
+        path: "*",
+        element: <NotFoundPage />
     },
     {
         path: "/sign-in",
