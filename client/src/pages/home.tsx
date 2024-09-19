3b189d5ee3753379eb773f7d112406549450cd31
tolgabayrakdev <bayraktolga28@gmail.com>
2024-09-19 13:32:00 +0300

    home page

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,6 +1,11 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>home</div>
+    <Flex justify="center" align="center" height="100vh" direction="column">
+      <Text fontSize="3xl">Hoşgeldin.</Text>
+      <Link style={{ textDecoration: 'underline' }} to="/sign-in">Giriş Yap</Link>
+    </Flex>
   )
 }
