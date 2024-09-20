302dc530990a670f053b741fcf97f5673ab03e37
tolgabayrakdev <bayraktolga28@gmail.com>
2024-09-20 17:46:15 +0300

    navige import

--- client/src/pages/not-found.tsx
+++ client/src/pages/not-found.tsx
@@ -1,5 +1,5 @@
 import { Box, Heading, Text, Button, VStack } from '@chakra-ui/react';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
     const navigate = useNavigate();
