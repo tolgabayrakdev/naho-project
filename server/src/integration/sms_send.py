21b7edd2a8f322148fdd7d340937fc4e5feb08c7
tolgabayrakdev <bayraktolga28@gmail.com>
2024-09-26 09:58:43 +0300

    fix

--- server/src/integration/sms_send.py
+++ server/src/integration/sms_send.py
@@ -5,7 +5,7 @@ url = "https://sandbox.api.mailtrap.io/api/send/3163160"
 
 payload = "{\"from\":{\"email\":\"hello@example.com\",\"name\":\"Mailtrap Test\"},\"to\":[{\"email\":\"tolgabayrakj@gmail.com\"}],\"subject\":\"You are awesome!\",\"text\":\"Congrats for sending test email with Mailtrap!\",\"category\":\"Integration Test\"}"
 headers = {
-  "Authorization": "Bearer 075f20653052674136e6b20a204c0899",
+  "Authorization": "Bearer ",
   "Content-Type": "application/json"
 }
 
